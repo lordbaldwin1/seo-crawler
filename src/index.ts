443b368b3e3ec0b8d6aaedf4159e07f96d87b913
zach <springerczachary@gmail.com>
2025-10-22 16:15:12 -0700

    some more initial server setup

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,15 @@
 import express from "express";
-import { middlewareLogResponses } from "./middleware";
+import { middlewareErrorHandler, middlewareLogResponses } from "./middleware";
 import { config } from "./config";
 
 const app = express();
 
 app.use(express.json());
 app.use(middlewareLogResponses);
+
+// handlers
+
+app.use(middlewareErrorHandler);
 
 const server = app.listen(config.port, () => {
     console.log(`listening on ${config.baseURL}:${config.port}`);
